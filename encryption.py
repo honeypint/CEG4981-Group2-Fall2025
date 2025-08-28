7a9d1cdecd707fc18b62be3a4c9a95d8fd24cf87
honeypint <122322065+honeypint@users.noreply.github.com>
2025-08-28 08:34:22 -0400

    Implemented md5 generation

--- encryption.py
+++ encryption.py
@@ -1,9 +1,21 @@
 from PIL import Image # Python Imaging Library
 import hashlib # hashlib for MD5
+import os # used for file iteration
 #from Crypto.Cipher import AES # pycrypto AES-128
 #from Crypto.Random import get_random_bytes # random bytes
 
-# TODO: make iterate through an entire image file
-image = Image.open('./sample-images/image13.png')
-md5hash = hashlib.md5(image.tobytes())
-print(md5hash)
+filepath = "./sample-images" # TODO: EDIT TO WHAT IS NEEDED
+imagehashes = []
+# Create md5 hashes for every image and pair them together in a list by [filename, md5]
+for file in os.listdir(filepath):
+    if file.endswith('.png'):
+        image = Image.open(filepath+'/'+file)
+        md5hash = hashlib.md5(image.tobytes())
+        md5hash = md5hash.hexdigest()
+        imagehashes.append([file, md5hash])
+        continue
+    else:
+        print("File "+file+" is not a .png")
+        continue
+for img in imagehashes:
+    print(img[0]+": hash "+img[1])
