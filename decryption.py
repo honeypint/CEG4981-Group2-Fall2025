27f279a1061253af4d3dc6a6cc1f881e7accb647
honeypint <122322065+honeypint@users.noreply.github.com>
2025-09-18 08:51:58 -0400

    Command for running cropping afterdecryption ends, spacing changes

--- decryption.py
+++ decryption.py
@@ -56,3 +56,9 @@ for file in os.listdir(SOURCE_DIR):
         decrypted_image.save(output_path, format=decrypted_image.format)
     else:
         continue # TODO: send over transmission error
+# -- Decryption process fully completed, send over to cropping program IF program file exists
+CROPPING_PROGRAM = "./Image-Cropping.py"
+if os.path.exists(CROPPING_PROGRAM):
+    os.system(f'python {CROPPING_PROGRAM}')
+else:
+    print(f'ERROR: {CROPPING_PROGRAM} does not exist! Stopping at Decryption step.')
