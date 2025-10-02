ec24e9156a2b109507a111323136fdec6f733fff
honeypint <122322065+honeypint@users.noreply.github.com>
2025-10-02 08:27:19 -0400

    Huge file structure changes to encryption side vs decryption side

--- Encryption Side/usb-detection.py
+++ Encryption Side/usb-detection.py
@@ -8,14 +8,14 @@ import shutil # file copying
 
 # -- Code that runs on a USB file input, checks every folder, and grabs JPG files.
 USERNAME = "Group2"
-os.makedirs("./USB_Images", exist_ok=True)
+os.makedirs("./USB_Images", exist_ok=True) # TODO: file path may change
 if os.path.isdir(f'/media/{USERNAME}'): # check if media folder exists first
     for filename in os.listdir(f'/media/{USERNAME}'): # get every file/folder in user's media folder
         file_path = f'/media/{USERNAME}/{filename}'
         if os.path.isdir(file_path): # if directory for usb - search it, grab all jpg files
             for file in os.listdir(file_path): 
                 if file.endswith(".png"):
-                    shutil.copy(os.path.join(file_path, file), f'./USB_Images') # copy to USB_Images folder
+                    shutil.copy(os.path.join(file_path, file), f'./USB_Images') # copy to USB_Images folder . TODO: file path may change
                     print(f'File {file} copied successfully!')
 
     # -- Decryption process fully completed, send over to cropping program IF program file exists
