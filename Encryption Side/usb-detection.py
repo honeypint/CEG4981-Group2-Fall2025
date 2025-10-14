88c75f7534e2d4987d8da3080c0c813988b7c2ce
honeypint <122322065+honeypint@users.noreply.github.com>
2025-10-14 08:31:45 -0400

    Adjust continual checks for USB insertion until png file found

--- Encryption Side/usb-detection.py
+++ Encryption Side/usb-detection.py
@@ -1,5 +1,6 @@
 import os # folder checking/management
 import shutil # file copying
+import time # for waiting
 
 # To think about:
 # /etc/udev/rules.d
@@ -10,18 +11,31 @@ import shutil # file copying
 USERNAME = "Group2"
 IMAGES_FOLDER = "./runImages"
 os.makedirs(IMAGES_FOLDER, exist_ok=True)
-if os.path.isdir(f'/media/{USERNAME}'): # check if media folder exists first
-    for filename in os.listdir(f'/media/{USERNAME}'): # get every file/folder in user's media folder
-        file_path = f'/media/{USERNAME}/{filename}'
-        if os.access(file_path, os.R_OK) and os.path.isdir(file_path): # if directory for usb - search it, grab all jpg files
-            for file in os.listdir(file_path): 
-                if file.endswith(".png"):
-                    shutil.copy(os.path.join(file_path, file), f'{IMAGES_FOLDER}') # copy to sample-images folder .
-                    print(f'File {file} copied successfully!')
 
-    # -- Decryption process fully completed, send over to cropping program IF program file exists
-    ENCRYPTION_PROGRAM = "./encryption.py"
-    if os.path.exists(ENCRYPTION_PROGRAM):
-        os.system(f'python {ENCRYPTION_PROGRAM}')
+def copy_usb():
+    result = False # start as false
+    if os.path.isdir(f'/media/{USERNAME}'): # check if media folder exists first
+        for filename in os.listdir(f'/media/{USERNAME}'): # get every file/folder in user's media folder
+            file_path = f'/media/{USERNAME}/{filename}'
+            if os.access(file_path, os.R_OK) and os.path.isdir(file_path): # if directory for usb - search it, grab all jpg files
+                for file in os.listdir(file_path): 
+                    if file.endswith(".png"):
+                        shutil.copy(os.path.join(file_path, file), f'{IMAGES_FOLDER}') # copy to sample-images folder .
+                        print(f'File {file} copied successfully!')
+                        result = True # adjust to true if a png is successfully found & copied!
+    return result # only returns a result once every file is looped through, to ensure obtainment of all files
+
+# -- Loops above function until files are copied
+loop = True
+while loop:
+    if copy_usb():
+        loop = False
     else:
-        print(f'ERROR: {ENCRYPTION_PROGRAM} does not exist! Stopping after Decryption step.')
+        print("Searching for a USB entry with PNG files in them...")
+    time.sleep(5)
+# -- Decryption process fully completed, send over to cropping program IF program file exists
+ENCRYPTION_PROGRAM = "./encryption.py"
+if os.path.exists(ENCRYPTION_PROGRAM):
+    os.system(f'python {ENCRYPTION_PROGRAM}')
+else:
+    print(f'ERROR: {ENCRYPTION_PROGRAM} does not exist! Stopping after USB Detection step.')
